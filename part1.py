881b57bdc2e8aefe1de1513f24fb93301752bedb
Anugrah hasandra <anugrahandra08@gmail.com>
2025-11-09 00:49:15 +0800

    Add files via upload

---
+++ part1.py
@@ -0,0 +1,21 @@
+# saya sedang belajar python
+# code di baca dari kiri ke kanan
+# code di baca dari atas ke bawah
+
+# cara untuk memindahkan satu baris, arahkan krusor pada baris yg ingin di pindahkan
+# tahan tombol alt, lalu tekan tombol arah bawah atau atas
+
+# cara untuk mengcopy satu baris full, sama dengan yg di atas
+ #namun ditambahkan tombol shift
+
+print("nama saya anugerah")
+print("saya adalah mahasiswa")
+print("smester awal")
+
+x = 9
+y = 6
+print(x * y)
+
+# Tugas
+# nama saya adalah : (Anugerah hasandra)
+# nim saya adalah : (25241102)
