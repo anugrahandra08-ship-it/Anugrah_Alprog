0e5434a4eb8a1be4b7cb199474577b7532bc9cae
Anugrah hasandra <anugrahandra08@gmail.com>
2025-11-18 07:51:02 +0800

    Add files via upload

---
+++ part3.py
@@ -0,0 +1,17 @@
+# kita akan belajar tipe data (jenis data)
+
+x = 10 # bilangan bulat (integar "int")
+print("nilai dari x :", x)
+print(type(x))
+
+y = 3.5 # bilangan pecahan (float "flo
+print("nilai dari y :", y)
+print(type(y))
+
+nama = "anugerah hasandra" # tipe data string (tulisan "str")
+print("nilai dari nama :",nama)
+print(type(nama))
+
+nim = "25241102" # tipe data string (tulisan "str")
+print("nilai dari nim :",nim)
+print(type(nim))
