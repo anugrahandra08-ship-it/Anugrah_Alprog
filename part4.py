91ce1c951056f33d4fb5fb12cc1b201e8d31f261
Anugrah hasandra <anugrahandra08@gmail.com>
2025-11-18 07:53:34 +0800

    Add files via upload

---
+++ part4.py
@@ -0,0 +1,13 @@
+# mengambil imputan dari user
+
+nama = input("anugerah hasandra")
+print("slamat datang ", nama)
+print(type(nama))
+
+umur = input("19")
+print("umur anda sekarang", 19)
+print(type(umur))
+
+kejujuran = bool(input("apakah kamu kuliah? :"))
+print("ooo kejujuranmu ", kejujuran)
+print(type(kejujuran))
