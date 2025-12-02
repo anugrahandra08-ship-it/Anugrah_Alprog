d6f05d8da3af3ce332933543908231fa9315c230
Anugrah hasandra <anugrahandra08@gmail.com>
2025-12-02 10:43:20 +0800

    Add files via upload

---
+++ tugaspert5 (1).py
@@ -0,0 +1,54 @@
+# Operasi Aritmatika 
+
+# Meminta input dari pengguna
+#penjumlahan
+a = 18
+b = 3
+hasil = a + b
+print(a, "ditambah", b, "=", hasil)
+
+#pengurangan
+a = 18
+b = 3
+hasil = a - b
+print(a, "dikurang", b, "=", hasil)
+
+#perkalian
+a = 18
+b = 3
+hasil = a * b
+print(a, "dikali", b, "=", hasil)
+
+#pembagian
+a = 18
+b = 3
+hasil = a / b
+print(a, "dibagi", b, "=", hasil)
+
+#perpangkatan
+a = 18
+b = 3
+hasil = a ** b
+print(a, "pangkat", b, "=", hasil)
+
+#modulus (sisa pemabagian)
+a = 18
+b = 3
+hasil = a % b
+print(a, "mod", b, "=", hasil)
+
+#floor division
+a = 18
+b = 3
+hasil = a // b
+print(a, "floor division", b, "=", hasil)
+
+#tugas menggabungkan seluruh operasi
+
+a = 8
+b = 6
+c = 3
+d = 4
+e = 2
+hasil = a + b  - c * c / d ** e % e // d
+print(a, "ditambah", b, "dikurang", c, "dikali", c, "dibagi", d, "pangkat", e, "mod", e, "floor division", d, "=", hasil)
