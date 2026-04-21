2ccd41534e8e4e0b59a7b5038fbbf3c7fb14f9fc
Anugrah hasandra <anugrahandra08@gmail.com>
2026-04-22 00:01:01 +0800

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,16 @@
+nama = input("masukan nama anda")
+
+if nama =="anugerah" :
+    print("selamat datang anugerah")
+umur = int(input("anugerah"))
+
+if umur == 20:
+    print("anda cukup umur")
+elif umur == 0 :
+    print("anda belum lahir")
+elif umur == 19 :
+    print("anda belum cukup umur")
+elif umur == 100 :
+    print("anda sudah tua.perbanyak ibadah")
+
+print("program selesai")
