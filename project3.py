449dd61730853aadd7d22ea79832e1d6eef0842a
Anugrah hasandra <anugrahandra08@gmail.com>
2026-04-22 00:03:21 +0800

    Add files via upload

---
+++ project3.py
@@ -0,0 +1,8 @@
+angka = int(input("masukan angka : "))
+
+while angka < -7 :
+    print("harus positif")
+    angka = int(input("masukan angka: "))
+
+print("angka yang di masukan adalah: ", angka)
+print("program selesai")
